6e0a634784f8ccf03e8764f1ee8fd6aa54408d46
singhakash7123 <vivek414viveksingh@gmail.com>
2026-02-18 21:49:17 +0530

    done

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   /* config options here */
+  output:"export",
   reactCompiler: true,
+  images:{
+    unoptimized:true,
+  },
+  basePath:"/Gaming_Website",
+
+  assetPrefix:"/Gaming_Website/"
 };
 
 export default nextConfig;
